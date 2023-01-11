71c47f8ed62da69f6bf0976ec99f734598a62675
Christopher Maltez <cflux.maltez@live.com>
2023-01-10 17:43:23 -0800

    finished part 1. very rough. moving on to part 2

--- routes/shipments.js
+++ routes/shipments.js
@@ -4,6 +4,10 @@ const express = require("express");
 const { BadRequestError } = require("../expressError");
 const router = new express.Router();
 
+const jsonschema = require("jsonschema");
+const orderSchema = require("../schemas/orderSchema.json");
+
+
 const { shipProduct } = require("../shipItApi");
 
 /** POST /ship
@@ -15,9 +19,18 @@ const { shipProduct } = require("../shipItApi");
  */
 
 router.post("/", async function (req, res, next) {
-  if (req.body === undefined) {
-    throw new BadRequestError();
+  if (req.body === undefined) throw new BadRequestError();
+
+  const shipment = req.body; // "shipment" is obect that contains properties.
+
+  const result = jsonschema.validate(
+    shipment, orderSchema, {required:true} )
+
+  if (!result.valid) {
+    const errs = result.errors.map(error => error.stack);
+    throw new BadRequestError(errs)
   }
+
   const { productId, name, addr, zip } = req.body;
   const shipId = await shipProduct({ productId, name, addr, zip });
   return res.json({ shipped: shipId });
--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -1,11 +1,17 @@
 "use strict";
 
+const shipItApi = require("../shipItApi") // <-- must be at very top
+shipItApi.shipProduct = jest.fn(); // <-- must be at very top
+
 const request = require("supertest");
 const app = require("../app");
 
+const jsonschema = require("jsonschema");
+const orderSchema = require("../schemas/orderSchema.json");
+
 
 describe("POST /", function () {
-  test("valid", async function () {
+  /* test("valid", async function () {
     const resp = await request(app).post("/shipments").send({
       productId: 1000,
       name: "Test Tester",
@@ -14,12 +20,37 @@ describe("POST /", function () {
     });
 
     expect(resp.body).toEqual({ shipped: expect.any(Number) });
-  });
+  }); */
 
   test("throws error if empty request body", async function () {
-    const resp = await request(app)
-      .post("/shipments")
-      .send();
+    const resp = await request(app).post("/shipments").send();
     expect(resp.statusCode).toEqual(400);
+  });
+
+  test("throws error if input is not valid", async function () {
+    const resp = await request(app).post("/shipments").send({
+      productId: 999,
+      name: "kaddem",
+      addr: "123 Main Street",
+      zip: "93012",
+    });
+    expect(resp.statusCode).toEqual(400);
+  });
+});
+
+
+describe("POST /shipments", function () {
+  test("valid", async function () {
+
+    shipItApi.shipProduct.mockReturnValue(1234);
+
+    const resp = await request(app).post("/shipments").send({
+      productId: 1000,
+      name: "kaddem",
+      addr: "123 Main Street",
+      zip: "93012",
+    });
+    expect(resp.body).toEqual({shipped: 1234})
+    // TODO: CHECK ERROR MESSAGES ARE COMING BACK CORRECTLY USING ".stack"
   });
 });
